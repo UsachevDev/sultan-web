923b89107d20e105f7ba69c7473d41ee10cbc02a
Usachev Ilya <109146918+UsachevDev@users.noreply.github.com>
2024-10-19 16:46:11 +0300

    feat: add PromoCarousel component

---
+++ src/app/components/NextJsCarousel/NextJsCarousel.js
@@ -0,0 +1,55 @@
+'use client'
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+
+const NextJsCarousel = () => {
+    return (
+        <div>
+            <Carousel
+                showArrows={true}
+                showIndicators={true}
+                showStatus={false}
+                infiniteLoop={true}
+                autoPlay={true}
+                interval={3000}
+                stopOnHover={true}
+                showThumbs={false}
+            >
+                <div className="slider" style={{backgroundImage: 'url("/data/image/carousel-image.png")'}}>
+                    <div className="content">
+                        <p className="promoDate">*АКЦИЯ ДЕЙСТВУЕТ ДО 04/09/22</p>
+                        <h2 className="promoTitle">Название Акции</h2>
+                        <p className="promoDescription">Условия акции в пару строк, Условия акции в пару строк, Условия акции в пару строк</p>
+                        <button className="promoButton">Принять участие</button>
+                    </div>
+                </div>
+                <div className="slider" style={{backgroundImage: 'url("/data/image/carousel-image.png")'}}>
+                    <div className="content">
+                        <p className="promoDate">*АКЦИЯ ДЕЙСТВУЕТ ДО 04/09/22</p>
+                        <h2 className="promoTitle">Название Акции</h2>
+                        <p className="promoDescription">Условия акции в пару строк, Условия акции в пару строк, Условия акции в пару строк</p>
+                        <button className="promoButton">Принять участие</button>
+                    </div>
+                </div>
+                <div className="slider" style={{backgroundImage: 'url("/data/image/carousel-image.png")'}}>
+                    <div className="content">
+                        <p className="promoDate">*АКЦИЯ ДЕЙСТВУЕТ ДО 04/09/22</p>
+                        <h2 className="promoTitle">Название Акции</h2>
+                        <p className="promoDescription">Условия акции в пару строк, Условия акции в пару строк, Условия акции в пару строк</p>
+                        <button className="promoButton">Принять участие</button>
+                    </div>
+                </div>
+                <div className="slider" style={{backgroundImage: 'url("/data/image/carousel-image.png")'}}>
+                    <div className="content">
+                        <p className="promoDate">*АКЦИЯ ДЕЙСТВУЕТ ДО 04/09/22</p>
+                        <h2 className="promoTitle">Название Акции</h2>
+                        <p className="promoDescription">Условия акции в пару строк, Условия акции в пару строк, Условия акции в пару строк</p>
+                        <button className="promoButton">Принять участие</button>
+                    </div>
+                </div>
+            </Carousel>
+        </div>
+    );
+};
+
+export default NextJsCarousel;
