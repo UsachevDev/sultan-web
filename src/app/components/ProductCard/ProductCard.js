f7978b8a1192a9c1ecfb18cd72d303a40b30197a
Stanislav <stas.zakaryan@gmail.com>
2024-10-27 18:26:16 +0300

    feat(ProductCard): show new properties

--- src/app/components/ProductCard/ProductCard.js
+++ src/app/components/ProductCard/ProductCard.js
@@ -1,27 +1,80 @@
-
+import Image from "next/image";
 import { ToBasketButton } from "../ToBasketButton/ToBasketButton";
 import "./ProductCard.css";
 
-export default function ProductCard({card: el}) {  
+function ShowInfo({info: filter}){
+    let text;
+    switch(filter)
+    {
+        case "discount": {
+            text = "СКИДКА";
+            break;
+        }
+        case "popular": {
+            text = "ПОПУЛЯРНОЕ";
+            break;
+        }
+        default: 
+            text = "";
+    }
+    if(filter != null){
+        return <span className={filter}>{text}</span>
+    };
+}
+
+function ShowSize({card: card})
+{
+    let size;
+    let type;
+    if(card.amount > 1)
+    {
+        size = card.amount + "x" + +(card.size/card.amount).toFixed(2);
+    }
+    else{
+        size = card.size;
+    }
+    let icon;
+    if(card.isLiquid)
+    {
+        icon = <Image src = "/icon/bottle-icon.svg" width = {9} height = {15} />;
+        type = "мл"
+    }
+    else{
+        icon = <Image src = "/icon/box-icon.svg" width = {20} height = {14} />;
+        type = "г"
+    }
+    
+    return <span className="product-size">{icon} {size} {type}</span>
+}
+
+export default function ProductCard({card: el, info: filter}) {  
+
     return (
         <div className="product-card">
             <div className="product-img">
-                <img src="http://localhost:3000/image/productCards/card0.png" width={92} />
+                <ShowInfo info={filter} />
+                <Image 
+                src={"/image/productCards/card" + el.id + ".svg"}
+                width={el.img.width}
+                height={el.img.height}
+                alt={el.img.alt}
+                />
+                <ShowSize card={el}/>
             </div>
-            <div key={el.id} className="product">
-                    <span className="product-name"><strong style={{fontWeight:800}}>{el.brandName}</strong> {el.productName}</span>
-                    <div className="product-desc">
-                        <ul>
-                            <li style={{fontWeight:500}}><span className="product-desc-name">Штрихкод: </span>{el.barCode}</li>
-                            <li><span className="product-desc-name">Производитель: </span>{el.manufacturer}</li>
-                            <li><span className="product-desc-name">Бренд: </span>{el.brandName}</li>
-                        </ul>
-                    </div>
-                    <div className="product-footer">
-                        <span className="product-price" style={{fontWeight:800}}>{el.price}₽</span>
-                        <span className="product-tobasket"><ToBasketButton /> </span>
-                    </div>
-
+            <div className="product">
+                <div className="product-name"><strong style={{fontWeight:800}}>{el.brand.name}</strong> {el.nameRu}</div>
+                <div className="product-desc">
+                    <ul>
+                        <li><span className="product-desc-name">Штрихкод: </span>{el.barcode}</li>
+                        <li><span className="product-desc-name">Производитель: </span>{el.manufacturer}</li>
+                        <li><span className="product-desc-name">Бренд: </span>{el.brand.name}</li>
+                    </ul>
                 </div>
+                <span className="product-footer">
+                    <span className="product-price" style={{fontWeight:800}}>{el.price}₽</span>
+                    <span className="product-tobasket"><ToBasketButton /> </span>
+                </span>
+                
+            </div>
         </div>
     )};
