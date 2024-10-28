6e7be1a87b616ff2ae4a9ad4b1b90c1ab0e7216a
Ilya Usachev <109146918+UsachevDev@users.noreply.github.com>
2024-10-28 21:19:09 +0300

    feat: add ContactMapSection component

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,26 @@
 import Footer from "./components/Footer/Footer";
 
 export default function Home() {
-  return (
-    <>
-      <h1 className="content">Главная страница</h1>
-      <Footer></Footer>
-    </>
-    
-  );
+    return (
+        <>
+            <main className="page-main">
+                <p>Просто тестовый контент</p>
+                <div>
+                    <p>Просто еще один тестовый контент</p>
+                    <p>Просто еще один тестовый контент</p>
+                </div>
+                <p>Просто тестовый контент</p>
+                <div>
+                    <p>Просто еще один тестовый контент</p>
+                    <p>Просто еще один тестовый контент</p>
+                </div>
+                <p>Просто тестовый контент</p>
+                <div>
+                    <p>Просто еще один тестовый контент</p>
+                    <p>Просто еще один тестовый контент</p>
+                </div>
+            </main>
+            <Footer></Footer>
+        </>
+    );
 }
