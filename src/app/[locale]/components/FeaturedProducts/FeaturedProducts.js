253b42e818055e9e75bae53994697e4a3b7e8084
Stanislav <stas.zakaryan@gmail.com>
2024-10-30 18:31:45 +0300

    fix: delete old params

--- src/app/[locale]/components/FeaturedProducts/FeaturedProducts.js
+++ src/app/[locale]/components/FeaturedProducts/FeaturedProducts.js
@@ -2,12 +2,12 @@ import "./FeaturedProducts.css";
 import Cards from "../Cards/Cards";
 
 
-export default function FeaturedProducts({leng: leng}){
+export default function FeaturedProducts(){
     return(
         <div className="featured-products">
             <div>
                 <h2 className="featured-products-head"><span>АКЦИОННЫЕ</span> ТОВАРЫ</h2>
-                <Cards info="popular" leng ={leng}/>   
+                <Cards info="popular"/>   
             </div>
             
         </div>
--- src/app/[locale]/page.js
+++ src/app/[locale]/page.js
@@ -2,12 +2,12 @@
 import {useTranslations} from 'next-intl';
 import FeaturedProducts from './components/FeaturedProducts/FeaturedProducts';
 
-export default function Home({params}) {
+export default function Home() {
   const t = useTranslations('HomePage');
   return (
     <>
         <h1>{t('title')}</h1>
-        <FeaturedProducts leng = {params.locale}/>
+        <FeaturedProducts />
     </>
 
   );
