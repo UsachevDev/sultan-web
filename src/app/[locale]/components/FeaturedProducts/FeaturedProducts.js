712b79879176a177a1c20bdb3746fc3c2cbd4a15
Stanislav <stas.zakaryan@gmail.com>
2024-10-30 20:27:29 +0300

    style(Cards component): update cards position

--- src/app/[locale]/components/FeaturedProducts/FeaturedProducts.js
+++ src/app/[locale]/components/FeaturedProducts/FeaturedProducts.js
@@ -4,7 +4,7 @@ import Cards from "../Cards/Cards";
 
 export default function FeaturedProducts(){
     return(
-        <div className="featured-products">
+        <div className="page-main">
             <div>
                 <h2 className="featured-products-head"><span>АКЦИОННЫЕ</span> ТОВАРЫ</h2>
                 <Cards info="popular"/>   
