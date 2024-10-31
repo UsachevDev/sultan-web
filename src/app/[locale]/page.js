9d2305f41f8103e5f48f3c7a3a12a37255309ef9
Закарян Станислав <stas.zakaryan@gmail.com>
2024-10-31 21:56:52 +0300

    fix: temporary header fix

--- src/app/[locale]/page.js
+++ src/app/[locale]/page.js
@@ -1,11 +1,10 @@
-"use client";
+
 import {useTranslations} from 'next-intl';
 import Footer from './components/Footer/Footer';
 import FeaturedProducts from './components/FeaturedProducts/FeaturedProducts';
 import PromoCarousel from './components/PromoCarousel/PromoCarousel';
 import CatalogCarousel from './components/CatalogCarousel/CatalogCarousel';
 import Header from './components/header/header';
-import ContactMapSection from './components/ContactMapSection/ContactMapSection';
 
 export default function Home() {
   const t = useTranslations('HomePage');
@@ -16,11 +15,7 @@ export default function Home() {
                <FeaturedProducts />
                <PromoCarousel />
                <CatalogCarousel />
-        <div>
-            <ContactMapSection />
-        </div>
         </main>
-        
         <Footer></Footer>
     </>
 
