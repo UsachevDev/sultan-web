7678ab58826a5ab11e3213aeb4d77fdc24603e25
Закарян Станислав <stas.zakaryan@gmail.com>
2024-11-02 20:45:40 +0300

    feat(Banner): add mobile version

--- src/app/[locale]/components/Banner/Banner.js
+++ src/app/[locale]/components/Banner/Banner.js
@@ -10,6 +10,7 @@ import ButtonTemplate from "../ButtonTemplate/ButtonTemplate";
 const Banner = () => {
 
     return (
+        <>
         <div className="banner">
             <div className="banner-blur" />
             <div className="banner-container" >
@@ -20,7 +21,7 @@ const Banner = () => {
                     <h2>ОПТОМ ПО КОКЧЕТАВУ И ОБЛАСТИ</h2>
                     <button className="btn-to-products"><Link href={"/products"}>В КАТАЛОГ</Link></button>
 
-                    <div className="banner-footer">
+                    <div className="banner-footer" id="banner-footer-1">
                         <ButtonTemplate width={39} height={39} text={"+"} className="btn-banner-plus"/>
                         <span>Только самые<br /> выгодные предложения</span>
                         <ButtonTemplate width={39} height={39} text={"+"} className="btn-banner-plus"/>
@@ -29,10 +30,24 @@ const Banner = () => {
                         </span>
                     </div>
                 </div>
+
             </div>
-                
+
+      
+        </div>
+        <div className="banner-footer-mobile" >
+                <ButtonTemplate width={18} height={18} text={"+"} className="btn-banner-plus"/>
+                <span>Только самые выгодные предложения</span> <br />
+                <ButtonTemplate width={18} height={18} text={"+"} className="btn-banner-plus"/>
+                <span>Бесплатная доставка
+                по <b>Кокчетаву от 10 тыс ₽</b>
+                </span>         
 
         </div>
+        </>
+       
+
+
     )
 }
 
--- src/app/[locale]/page.js
+++ src/app/[locale]/page.js
@@ -4,19 +4,18 @@ import Footer from './components/Footer/Footer';
 import FeaturedProducts from './components/FeaturedProducts/FeaturedProducts';
 import PromoCarousel from './components/PromoCarousel/PromoCarousel';
 import CatalogCarousel from './components/CatalogCarousel/CatalogCarousel';
+import Banner from './components/Banner/Banner';
+import ContactMapSection from './components/ContactMapSection/ContactMapSection';
 import Header from './components/header/header';
 
 export default function Home() {
   const t = useTranslations('HomePage');
   return (
     <>
-        <Header />
-        <main className="page-main">
-               <FeaturedProducts />
-               <PromoCarousel />
-               <CatalogCarousel />
-        </main>
-        <Footer></Footer>
+      <Banner />
+      <main className='page-main'>
+        <FeaturedProducts />
+      </main>
     </>
 
   );
