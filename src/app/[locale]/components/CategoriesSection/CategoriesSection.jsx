c1fc5dcc7fa60a60f5b4d4d81a9f51697012370b
Ilya Usachev <109146918+UsachevDev@users.noreply.github.com>
2024-11-04 12:53:14 +0300

    chore: change code style

--- src/app/[locale]/components/CategoriesSection/CategoriesSection.jsx
+++ src/app/[locale]/components/CategoriesSection/CategoriesSection.jsx
@@ -1,20 +1,20 @@
 "use client"
 import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
-import styles from './CategoriesSection.scss'; // Импортируем SCSS стили
+import styles from './CategoriesSection.scss';
 
 const CategoriesSection = () => {
-    const [title, setTitle] = useState({}); // Состояние для заголовка
-    const [subtitle, setSubtitle] = useState(''); // Состояние для подписи
+    const [title, setTitle] = useState({});
+    const [subtitle, setSubtitle] = useState('');
     const [items, setItems] = useState([]);
 
     useEffect(() => {
         const fetchCategories = async () => {
-            const response = await fetch('/data/categoriesData.json'); // Укажите путь к вашему JSON файлу
+            const response = await fetch('/data/categoriesData.json');
             const data = await response.json();
-            setTitle(data.title); // Устанавливаем заголовок из JSON
-            setSubtitle(data.subtitle); // Устанавливаем подпись из JSON
-            setItems(data.items); // Устанавливаем категории
+            setTitle(data.title);
+            setSubtitle(data.subtitle);
+            setItems(data.items);
         };
 
         fetchCategories();
