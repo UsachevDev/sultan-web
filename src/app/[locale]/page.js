4c625bd75491785ef66a65e3ceb5c6db424fda59
Закарян Станислав <stas.zakaryan@gmail.com>
2024-11-13 16:30:58 +0300

    chore(Banner,Products): add index.js

--- src/app/[locale]/page.js
+++ src/app/[locale]/page.js
@@ -1,7 +1,7 @@
-import FeaturedProducts from './components/FeaturedProducts/FeaturedProducts';
+import FeaturedProducts from './components/FeaturedProducts';
 import PromoCarousel from './components/PromoCarousel/PromoCarousel';
 import CatalogCarousel from './components/CatalogCarousel/CatalogCarousel';
-import Banner from './components/Banner/Banner';
+import Banner from "./components/Banner";
 import ContactMapSection from './components/ContactMapSection/ContactMapSection';
 import CategoriesSection from './components/CategoriesSection/CategoriesSection';
 
