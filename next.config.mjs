24ff3c5f2b58fbb87a4bb9df5c5067c07d1b9eac
Usachev Ilya <109146918+UsachevDev@users.noreply.github.com>
2024-11-16 11:23:13 +0300

    deploy: change next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,14 @@
 import createNextIntlPlugin from 'next-intl/plugin';
- 
+
 const withNextIntl = createNextIntlPlugin();
- 
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
- 
+const nextConfig = {
+    basePath: "/sultan-web",
+    output: "export",
+    reactStrictMode: true,
+};
+
+module.exports = nextConfig;
+
 export default withNextIntl(nextConfig);
