507c955eecfa719c947ce391f3e36f4c94505573
Закарян Станислав <stas.zakaryan@gmail.com>
2024-11-17 13:18:01 +0300

    refactor(Banner): render promo with .map()

--- src/app/[locale]/components/Banner/Banner.js
+++ src/app/[locale]/components/Banner/Banner.js
@@ -3,56 +3,47 @@
 import { Link } from "@/i18n/routing";
 import { useTranslations } from "next-intl";
 import useWidth from "../../core/hooks/useWidth";
+import { elemPlus, PromoContent } from "./BannerPromo";
 import ButtonUI from "../UI/ButtonUI/ButtonUI";
 
 import "./Banner.scss";
 
 const Banner = () => {
     const { isMobile, isMobileSmall } = useWidth();
-
-    const btnPlus = <ButtonUI
-        label="+"
-        size="xs"
-        className="btn-banner-plus"
-    />
-
+    const promoData = PromoContent();
     const t = useTranslations("Banner");
     return (
         <>
             {!isMobile && !isMobileSmall
                 ?
-                <>
-                    <div className="banner">
-                        <div className="banner-blur" />
-                        <div className="banner-container" >
-                            <div className="banner-content">
-                                <h1>
-                                    {t('h1.firstLine')}<br /> {t('h1.secondLine')}
-                                </h1>
-                                <h2>{t('h2')}</h2>
-                                <Link href={"/products"}>
-                                    <ButtonUI
-                                        label={t('button')}
-                                        size="lg"
-                                        className="btn-catalog">
-                                    </ButtonUI>
-                                </Link>
-                                <div className="banner-footer" id="banner-footer-1">
-                                    {btnPlus}
+                <div className="banner">
+                    <div className="banner-blur" />
+                    <div className="banner-container" >
+                        <div className="banner-content">
+                            <h1>
+                                {t('h1.firstLine')}<br /> {t('h1.secondLine')}
+                            </h1>
+                            <h2>{t('h2')}</h2>
+                            <Link href={"/products"}>
+                                <ButtonUI
+                                    label={t('button')}
+                                    size="lg"
+                                    className="btn-catalog">
+                                </ButtonUI>
+                            </Link>
+                            <div className="banner-footer">
+                                {promoData.map((el) => (
                                     <span className="banner-footer-promo">
-                                        {t('promo.span1')}<br /> {t('promo.span2')}
+                                        {elemPlus}
+                                        <p>{el.span}</p>
                                     </span>
-                                    {btnPlus}
-                                    <span className="banner-footer-promo">
-                                        {t('promo.span3')}<br /> <b>{t('promo.span4')}</b>
-                                    </span>
-                                </div>
+                                ))}
                             </div>
                         </div>
                     </div>
-                </>
+                </div>
                 :
-                <>
+                <section className="banner-mobile">
                     <div className="banner">
                         <div className="banner-blur" />
                         <div className="banner-container" >
@@ -66,16 +57,14 @@ const Banner = () => {
                         </div>
                     </div>
                     <div className="banner-footer-mobile" >
-                        <span>
-                            {btnPlus}
-                            {t('promo.span1')} {t('promo.span2')} <br />
-                        </span>
-                        <span>
-                            {btnPlus}
-                            {t('promo.span3')} <b>{t('promo.span4')}</b>
-                        </span>
+                        {promoData.map((el) => (
+                            <span className="banner-footer-promo">
+                                {elemPlus}
+                                <p>{el.span}</p>
+                            </span>
+                        ))}
                     </div>
-                </>
+                </section>
             }
         </>
     )
---
+++ src/app/[locale]/components/Banner/BannerPromo.js
@@ -0,0 +1,24 @@
+"use client";
+
+import { useTranslations } from "next-intl";
+
+
+export const elemPlus = <span className="banner-footer-plus">+</span>;
+
+
+
+export const PromoContent = () => {
+    const t = useTranslations("Banner");
+    return(
+        [
+            {
+                "span": t('promo.span1')
+            },
+            {
+                "span": t('promo.span2')
+            }
+        ]
+    )
+} 
+
+export default PromoContent;
