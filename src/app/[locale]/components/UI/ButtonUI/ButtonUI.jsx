7fe6d071eba472dedba30e723abe4476c723aaa7
Закарян Станислав <stas.zakaryan@gmail.com>
2024-11-18 17:18:16 +0300

    chore(ButtonUI): remove waste params

--- src/app/[locale]/components/UI/ButtonUI/ButtonUI.jsx
+++ src/app/[locale]/components/UI/ButtonUI/ButtonUI.jsx
@@ -1,13 +1,3 @@
-// @param {Object} children - Dom elements
-// @param {string} type - Values: button, submit, reset
-// @param {string} size - Values: xs, md, lg
-// @param {string} theme - Values: default, accent, highlight
-// @param {string} label
-// @param {string} icon
-// @param {boolean} disabled
-// @param {function} onClick
-// @returns
-
 import { useRef } from "react";
 import Icons from "../Icons/Icons";
 import "./ButtonUI.scss";
@@ -19,16 +9,12 @@ const ButtonUI = ({
     label = "",
     icon = "",
     size = "md",
-    theme = "default",
     disabled = false,
     onClick,
 }) => {
-    const buttonRef = useRef(null);
-
     return (
         <button
-            ref={buttonRef}
-            className={`ui-button ui-button--${size} ui-button--${theme} ${className}`}
+            className={`ui-button ui-button--${size} ${className}`}
             type={type}
             disabled={disabled}
             onClick={onClick}
