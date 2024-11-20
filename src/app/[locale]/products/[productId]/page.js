65a82df62ae4fec522f95300ffb20e98ea55b795
Закарян Станислав <stas.zakaryan@gmail.com>
2024-11-20 11:22:41 +0300

    chore: disable [product] page for deploy

--- src/app/[locale]/products/[productId]/page.js
+++ src/app/[locale]/products/[productId]/page.js
@@ -1,20 +1,19 @@
 
-async function getProducts() {
-    const res = await fetch('http://localhost:3000/data/products.json');
-    const resJson = await res.json();
-    return resJson;
-}
+// async function getProducts() {
+//     const resJson = await res.json();
+//     return resJson;
+// }
 
-export default async function Product({params}){
-    const products = await getProducts();
-    const product = products[params.productId];
+// export default async function Product({params}){
+//     const products = await getProducts();
+//     const product = products[params.productId];
 
-    return (
-        <div>
-            <h1>{product.productName}</h1><br/>
-            <p>{product.description}</p><br/>
-            <p>Price: {product.price}</p>
+//     return (
+//         <div>
+//             <h1>{product.productName}</h1><br/>
+//             <p>{product.description}</p><br/>
+//             <p>Price: {product.price}</p>
 
-        </div>
-    )
-}
+//         </div>
+//     )
+// }git
