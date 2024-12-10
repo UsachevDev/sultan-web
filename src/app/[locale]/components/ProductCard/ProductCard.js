677cef42d3dcdf4d606d47bf3de8c1b17ac06b6a
Ilya Usachev <109146918+UsachevDev@users.noreply.github.com>
2024-12-10 20:16:57 +0300

    style: add uppercase to button in styles

--- src/app/[locale]/components/ProductCard/ProductCard.js
+++ src/app/[locale]/components/ProductCard/ProductCard.js
@@ -5,59 +5,98 @@ import ButtonUI from "../UI/ButtonUI/ButtonUI";
 import Icon from "../UI/Icons/Icons";
 import "./ProductCard.scss";
 
-export function ShowProductSize({ card: card, t: t, className: name, disableIcon = false}) {
+export function ShowProductSize({
+    card: card,
+    t: t,
+    className: name,
+    disableIcon = false,
+}) {
     let size = +(card.size / card.amount).toFixed(2);
     if (card.amount > 1) {
         size = card.amount + "x" + size;
     }
 
     let icon, type;
-    card.isLiquid 
-        ? (icon = <Icon name="bottle"/>, type = t('liquidType'))
-        : (icon = <Icon name="box"/>, type = t('type'))
+    card.isLiquid
+        ? ((icon = <Icon name="bottle" />), (type = t("liquidType")))
+        : ((icon = <Icon name="box" />), (type = t("type")));
 
     return (
         <span className={name}>
             {!disableIcon && icon}
             {size}
             <b>{type}</b>
-        </span>);
+        </span>
+    );
 }
 
-export default function ProductCard({ card: el }) {
+export default function ProductCard({ card: element }) {
     const t = useTranslations("ProductCard");
-    const productName = t("productName") === "ru" ? el.nameRu : el.nameEn;
+    const productName =
+        t("productName") === "ru" ? element.nameRu : element.nameEn;
     return (
-            <article className="product-card">
-                <Link href={"/products/" + el.id}>
-                    <div className="product-img">
-                        {el.isPopular && (
-                            <span className="popular">{t("popular")}</span>
-                        )}
-                        <Image
-                            src={"/image/productCards/card" + el.id + ".svg"}
-                            fill={true}
-                            alt={productName}
-                        />
-                        <ShowProductSize card={el} t={t} className="product-size"/>
+        <article className="product-card">
+            <Link href={`/products/${element.id}`}>
+                <div className="product-img">
+                    {element.isPopular && (
+                        <span className="popular">{t("popular")}</span>
+                    )}
+                    <Image
+                        src={"/image/productCards/card" + element.id + ".svg"}
+                        fill={true}
+                        alt={productName}
+                    />
+                    <ShowProductSize
+                        card={element}
+                        t={t}
+                        className="product-size"
+                    />
+                </div>
+                <div className="product">
+                    <div className="product-name">
+                        <strong style={{ fontWeight: 800 }}>
+                            {element.brand.name}{" "}
+                        </strong>
+                        {productName}
                     </div>
-                    <div className="product">
-                        <div className="product-name">
-                            <strong style={{ fontWeight: 800 }}>{el.brand.name} </strong>
-                            {productName}</div>
-                        <div className="product-desc">
-                            <ul>
-                                <li><span className="product-desc-name">{t("barcode")}: </span>{el.barcode}</li>
-                                <li><span className="product-desc-name">{t("manufacturer")}: </span>{el.manufacturer}</li>
-                                <li><span className="product-desc-name">{t("brand")}: </span>{el.brand.name}</li>
-                            </ul>
-                        </div>
-                        <span className="product-footer">
-                            <span className="product-price" style={{ fontWeight: 800 }}>{el.price}₽</span>
-                            <ButtonUI icon="basket" size="sm" label={t('button').toUpperCase()} className="btn-tocart" />
+                    <div className="product-desc">
+                        <ul>
+                            <li>
+                                <span className="product-desc-name">
+                                    {t("barcode")}:{" "}
+                                </span>
+                                {element.barcode}
+                            </li>
+                            <li>
+                                <span className="product-desc-name">
+                                    {t("manufacturer")}:{" "}
+                                </span>
+                                {element.manufacturer}
+                            </li>
+                            <li>
+                                <span className="product-desc-name">
+                                    {t("brand")}:{" "}
+                                </span>
+                                {element.brand.name}
+                            </li>
+                        </ul>
+                    </div>
+                    <span className="product-footer">
+                        <span
+                            className="product-price"
+                            style={{ fontWeight: 800 }}
+                        >
+                            {element.price}₽
                         </span>
-                    </div>
-                </Link>
-            </article>
-    )
-};
+                        <ButtonUI
+                            icon="basket"
+                            size="sm"
+                            label={t("button")}
+                            className="btn-tocart"
+                        />
+                    </span>
+                </div>
+            </Link>
+        </article>
+    );
+}
