8825e70b5f93dd142c38aab85bc84740d7b456bf
Ilya Usachev <109146918+UsachevDev@users.noreply.github.com>
2024-12-20 17:19:30 +0300

    style(Products): add styles for filter component

--- src/app/[locale]/components/FilterSidebar/FilterSidebar.jsx
+++ src/app/[locale]/components/FilterSidebar/FilterSidebar.jsx
@@ -1,3 +1,5 @@
+import ButtonUI from "UI/ButtonUI/ButtonUI.jsx";
+import Accordion from "../UI/Accordion/Accordion";
 import "./FilterSidebar.scss";
 
 const FilterSidebar = () => {
@@ -6,50 +8,110 @@ const FilterSidebar = () => {
             <section className="filter-section">
                 <h2 className="filter-title">Подбор по параметрам</h2>
                 <div className="price-filter">
-                    <label for="price-title"><span className="price-title-span">Цена</span> ₸</label>
+                    <label for="price-title">
+                        <span className="price-title-span">Цена</span> ₸
+                    </label>
                     <div className="price-inputs">
-                        <input type="number" id="price-min" placeholder="0" min="0" />
+                        <input
+                            type="number"
+                            id="price-min"
+                            placeholder="0"
+                            min="0"
+                        />
                         <span>-</span>
-                        <input type="number" id="price-max" placeholder="10 000" min="0" />
+                        <input
+                            type="number"
+                            id="price-max"
+                            placeholder="10 000"
+                            min="0"
+                        />
                     </div>
                 </div>
             </section>
-
 
             <section className="filter-section">
                 <h3 className="filter-subtitle">Производитель</h3>
                 <div className="search-box">
                     <input type="text" placeholder="Поиск..." />
-                    <button className="search-button">🔍</button>
+                    <ButtonUI
+                        className="search-button"
+                        icon="search"
+                        size="xs"
+                    />
                 </div>
                 <ul className="filter-list">
-                    <li><label><input type="checkbox" /> Grifon (56)</label></li>
-                    <li><label><input type="checkbox" /> Boyscout (166)</label></li>
-                    <li><label><input type="checkbox" /> Paclan (21)</label></li>
-                    <li><label><input type="checkbox" /> Булгари Грин (21)</label></li>
+                    <li>
+                        <label>
+                            <input type="checkbox" /> Grifon (56)
+                        </label>
+                    </li>
+                    <li>
+                        <label>
+                            <input type="checkbox" /> Boyscout (166)
+                        </label>
+                    </li>
+                    <li>
+                        <label>
+                            <input type="checkbox" /> Paclan (21)
+                        </label>
+                    </li>
+                    <li>
+                        <label>
+                            <input type="checkbox" /> Булгари Грин (21)
+                        </label>
+                    </li>
                 </ul>
-                <button className="show-more">Показать все</button>
+                <Accordion
+                    title="Показать все"
+                    className="show-more"
+                ></Accordion>
             </section>
 
             <section className="filter-section">
                 <h3 className="filter-subtitle">Бренд</h3>
                 <div className="search-box">
                     <input type="text" placeholder="Поиск..." />
-                    <button className="search-button">🔍</button>
+                    <ButtonUI icon="search" size="xs" />
                 </div>
                 <ul className="filter-list">
-                    <li><label><input type="checkbox" /> Nivea (56)</label></li>
-                    <li><label><input type="checkbox" /> GRIFON (66)</label></li>
-                    <li><label><input type="checkbox" /> Домашний сундук (166)</label></li>
-                    <li><label><input type="checkbox" /> HELP (21)</label></li>
+                    <li>
+                        <label>
+                            <input type="checkbox" /> Nivea (56)
+                        </label>
+                    </li>
+                    <li>
+                        <label>
+                            <input type="checkbox" /> GRIFON (66)
+                        </label>
+                    </li>
+                    <li>
+                        <label>
+                            <input type="checkbox" /> Домашний сундук (166)
+                        </label>
+                    </li>
+                    <li>
+                        <label>
+                            <input type="checkbox" /> HELP (21)
+                        </label>
+                    </li>
                 </ul>
-                <button className="show-more">Показать все</button>
+                <Accordion
+                    title="Показать все"
+                    className="show-more"
+                ></Accordion>
             </section>
 
-
             <div className="filter-actions">
-                <button className="apply-button">Показать</button>
-                <button className="reset-button">🗑</button>
+                <ButtonUI
+                    className="apply-button"
+                    label="Показать"
+                    size="m"
+                />
+                <ButtonUI
+                    className="reset-button"
+                    icon="recycle-bin"
+                    size="xs"
+                />
             </div>
         </div>
     );
