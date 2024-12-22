02afc7ced442b6d86c906d69ab61b6e87a8426d4
Nikolai <chaykin.kolya@list.ru>
2024-12-23 00:10:30 +0300

    add basket items to basket page

--- src/app/[locale]/basket/page.js
+++ src/app/[locale]/basket/page.js
@@ -1,5 +1,20 @@
-export default function Basket(){
-    return(
-        <h1>Basket</h1>
-    )
+"use client";
+
+import { useBasket } from "../core/context/BasketContext";
+import ProductBasket from "../components/ProductBasket/ProductBasket";
+
+export default function Basket() {
+    const { basket } = useBasket();
+
+    if (basket.length === 0) {
+        return <p>Ваша корзина пуста.</p>;
+    }
+
+    return (
+        <>
+            {basket.map((product) => (
+                <ProductBasket key={product.id} card={product} />
+            ))}
+        </>
+    );
 }
